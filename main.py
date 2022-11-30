7214a92c159bed2bc8076ce52c27f3206cefdfbc
Nyleon <119215418+Nyleon@users.noreply.github.com>
2022-11-30 22:55:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,16 +18,19 @@ for event in bot.longpoll.listen():
                     for user_vk_id in users_vk_id:
                         if not select_of_table(user_id, user_vk_id):
                             user_vk_link = 'vk.com/id' + str(user_vk_id)
-                            user_photos = user_vk.get_photo(user_vk_id)
+                            user_photos = user_vk.get_photo(user_vk_id,user_id)
                             bot.send_msg(user_id, user_vk_link)
                             add_in_table(user_id, user_vk_id)
-                            for i in user_photos:
-                                bot.send_photo(user_id, user_vk_id, i[0])
-                            keyboard = VkKeyboard()
-                            keyboard.add_button('Далее', VkKeyboardColor.POSITIVE)
-                            bot.send_but(event.user_id,
-                                         f'Чтобы получить новую анкету, нажмите: Далее',
-                                         keyboard)
+                            try:
+                                for i in user_photos:
+                                    bot.send_photo(user_id, user_vk_id, i[0])
+                                keyboard = VkKeyboard()
+                                keyboard.add_button('Далее', VkKeyboardColor.POSITIVE)
+                                bot.send_but(event.user_id,
+                                             f'Чтобы получить новую анкету, нажмите: Далее',
+                                             keyboard)
+                            except Exception:
+                                break
                             for event in bot.longpoll.listen():
                                 if event.type == VkEventType.MESSAGE_NEW and event.to_me:
                                     request = event.text
